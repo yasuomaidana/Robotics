af72d5bba0701ffcb4b263f9e991bb50f06b2348
yasuomaidana <yasuomaidana@gmail.com>
2024-05-17 18:27:24 +0900

    Generating radial omnidirectional robot

---
+++ Control of Ground And Aerial Robots/kinematic_models/wheeled_robots/omnidirectional_robots/radial_omnidirectional_robot.py
@@ -0,0 +1,31 @@
+from matplotlib import patches
+from numpy import radians, cos, sin
+
+from omnidirectional_robot import OmnidirectionalRobot
+from omni_wheel import OmniWheel
+
+
+class RadialOmnidirectionalRobot(OmnidirectionalRobot):
+    def __init__(self, num_wheels, radius, wheel_width=None, wheel_diameter=0.5, axis_rotation=0,
+                 velocity_color='orange', wheel_color='black'):
+        super().__init__(num_wheels, velocity_color, wheel_color)
+        self.radius = radius
+        self.axis_rotation = axis_rotation
+        self.wheel_width = wheel_width or radius / 2
+        self.wheel_diameter = wheel_diameter
+        self.generate_wheels()
+
+    def generate_wheels(self):
+        angle_step = 360 / self.num_wheels
+        for i in range(self.num_wheels):
+            angle = i * angle_step + self.axis_rotation
+            x = self.radius * cos(radians(angle))
+            y = self.radius * sin(radians(angle))
+            wheel = OmniWheel(distance_from_robot_frame=(x, y), orientation=angle, diameter=self.wheel_diameter,
+                              width=self.wheel_width)
+            self.add_wheel(wheel)
+
+    def plot(self, ax):
+        super().plot(ax)
+        circle = patches.Circle((0, 0), self.radius, edgecolor='black', facecolor='none')
+        ax.add_patch(circle)
